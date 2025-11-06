40a8dab89a3acf1f5ad7a3cc2aafccb7b21cdbfa
Abdulla <abdusattorovabdulla60@gmail.com>
2025-11-06 11:56:49 +0500

    add

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -115,7 +115,7 @@ export default function Home() {
                       </SelectTrigger>
                       <SelectContent>
                         <SelectItem value="AD">AD</SelectItem>
-                        <SelectItem value="AD">AC</SelectItem>
+                        <SelectItem value="AC">AC</SelectItem>
                         <SelectItem value="AE">AE</SelectItem>
                         <SelectItem value="AB">AB</SelectItem>
                         <SelectItem value="KA">KA</SelectItem>
